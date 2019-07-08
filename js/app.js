33cd2b08a4d704978748e374f7551e37008c0500
dotkalm <joelmholmberg@gmail.com>
2019-07-08 16:12:38 -0700

    key controls

--- js/app.js
+++ js/app.js
@@ -1,23 +1,49 @@
+
+
 const goFishing = (() =>{
     const title = document.querySelector('.titleScreen');
-    const titleScreen = document.createElement('p')
+    const titleScreen = document.createElement('h1')
     titleScreen.innerText = 'Stardew Fishing';
     title.appendChild(titleScreen);
 })
 
 
-const fishingSlider = (()=>{
+const fishingPole = (()=>{
+    let poleY = 0;
     const net = document.querySelector('#innerRect');
-    let pos = .01;
     const animate = setInterval(() =>{
-        pos += .01;
-        net.setAttribute('style', `transform:translateY(${pos}rem)`)
-        if(pos >= 18.5){
-            pos = 0;
+        poleY += .01;
+        net.setAttribute('style', `transform:translateY(${poleY}rem)`)
+        if(poleY >= 18.5){
+            poleY = 0;
         }
-    }, 10)
-    
+    }, 10)  
+    window.onkeyup = function(e) {
+        const key = e.keyCode ? e.keyCode : e.which;
+        if (key == 40) {
+            poleY += .25;
+            console.log('up')
+        }else if (key == 38) {
+            poleY -= .25;
+            console.log('down')
+        }
+    }
 })
 
-fishingSlider();
+const fishOnHook = (() => {
+    const fish = document.querySelector('#fish');
+    let fishPosition = 17.2
+    const animate = setInterval(() => {
+        fishPosition -= .01;
+        fish.setAttribute('style', `transform:translateY(${fishPosition}rem)`)
+        if(fishPosition <= 0){
+            fishPosition = 17.2;
+        }
+    }, 20)
+})
+
+fishingPole();
 goFishing();
+fishOnHook();
+
+
