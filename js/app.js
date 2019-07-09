086729d69bc81c96aa33a4eca63d1001648b1aab
dotkalm <joelmholmberg@gmail.com>
2019-07-08 21:33:00 -0700

    fish stats

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
 let fishPosition = 290
 
 const fishingPole = (()=>{
-    let poleY = 0;
+    let poleY = -1;
     const net = document.querySelector('#innerRect');
     const animate = setInterval(() =>{
-        poleY += .1;
+        poleY += .2;
         net.setAttribute('style', `transform:translateY(${poleY}px)`)
         if(poleY >= 336){
             poleY = 0;
         }
-    }, 5)  
+    }, 10)  
+
     window.onkeyup = function(e) {
         const key = e.keyCode ? e.keyCode : e.which;
         if (key == 40) {
             poleY += 5;
-            console.log('up')
         }else if (key == 38) {
             poleY -= 5;
-            console.log('down')
         }
     }
 })
@@ -32,6 +31,24 @@ const fishOnHook = (() => {
         }
     }, 10)
 })
+{/* <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="100px" height="2500px" viewBox="0 0 100 2500">
+<rect fill="#ffffff" width="100" height="2000"/>
+</svg> */}
+const fishStats = (() => {
+    const fishStats = document.querySelector('.fishStatsBox');
+    const element = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    const statsBox = document.querySelector('svg');
+    element.setAttribute('fill', '#ffffff')
+    element.setAttribute('width', '100px')
+    element.setAttribute('height', `200px`)
+    statsBox.setAttribute('width', '100%')
+    fishStats.appendChild(statsBox);
+    statsBox.appendChild(element);
+    const moveFish = setInterval(() => {
+        element.setAttribute('height', `${fishPosition}px`)
+    }, 30)
+})
 const hookedAlert = (() =>{
     const title = document.querySelector('.titleScreen');
     const titleScreen = document.createElement('h1')
@@ -45,5 +62,5 @@ const hookedAlert = (() =>{
 fishingPole();
 hookedAlert();
 fishOnHook();
-
+fishStats();
 
